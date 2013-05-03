62b3537696e9fc939a9679989ff2a202f562bf30
hemdawgz <h292zhan@uwaterloo.ca>
2013-05-03 17:13:28 -0400

    preliminary setup on main.php

--- thestuff/main.php
+++ thestuff/main.php
@@ -1,10 +1,46 @@
+<?php
+
+//All dummy values for now
+$busyTime = 0;
+$deadlnTime = 1369688400;
+$timeLeft = ($deadlnTime - time() - $busyTime);
+
+//These are the things that actually get printed out
+$deadlnTimeStr = "before " . date("g:i a", $deadlnTime) . ",";
+
+$deadlnDateStr = date("M d, Y", $deadlnTime);
+
+if ($timeLeft < 3600) {
+	$timeLeftStr = floor($timeLeft/60) . " minutes";
+}
+else {
+	$timeLeftStr = floor($timeLeft/3600) . " hours";
+}
+
+$pagecontents = <<<EOPAGE
 <!DOCTYPE html>
 <head>
 	<link type="text/css" rel="stylesheet" href="stylesheet.css" />
+	<title>Sixteenth Night</title>
 </head>
 <body>
-	<p><?php
-		echo "durr durr";
-		?>
+	<p class="static">
+		You have
+	</p>
+	<p id="time_left">
+		${timeLeftStr}
+	</p>
+	<p class="static">
+		of free time remaining
+	</p>
+	<p id="deadln_time">
+		${deadlnTimeStr}
+	</p>
+	<p id="deadln_date">
+		${deadlnDateStr}
 	</p>
 </body>
+EOPAGE;
+echo $pagecontents;
+
+?>
