5fa5abd60c01dc01234b1e7b1606976de3db5352
hemdawgz <h292zhan@uwaterloo.ca>
2013-05-03 23:48:00 -0400

    preliminary styling

--- thestuff/main.php
+++ thestuff/main.php
@@ -1,12 +1,14 @@
 <?php
 
 //All dummy values for now
+$GMTOffset= -4;
+$adjustedTime = time() + (3600 * $GMTOffset);
 $busyTime = 0;
-$deadlnTime = 1369688400;
-$timeLeft = ($deadlnTime - time() - $busyTime);
+$deadlnTime = 1367798400;
+$timeLeft = ($deadlnTime - $adjustedTime - $busyTime);
 
 //These are the things that actually get printed out
-$deadlnTimeStr = "before " . date("g:i a", $deadlnTime) . ",";
+$deadlnTimeStr = date("g:i a", $deadlnTime);
 
 $deadlnDateStr = date("M d, Y", $deadlnTime);
 
@@ -21,24 +23,36 @@ $pagecontents = <<<EOPAGE
 <!DOCTYPE html>
 <head>
 	<link type="text/css" rel="stylesheet" href="stylesheet.css" />
+	<link href="http://fonts.googleapis.com/css?family=Arvo:400,700" rel='stylesheet' type='text/css' />
 	<title>Sixteenth Night</title>
 </head>
 <body>
-	<p class="static">
-		You have
-	</p>
-	<p id="time_left">
-		${timeLeftStr}
-	</p>
-	<p class="static">
-		of free time remaining
-	</p>
-	<p id="deadln_time">
-		${deadlnTimeStr}
-	</p>
-	<p id="deadln_date">
-		${deadlnDateStr}
-	</p>
+	<div id="maintext">
+		<div id="header">
+			You have
+		</div>
+		<div id="time_left"><strong>
+			${timeLeftStr}
+		</strong></div>
+		<div id="static">
+			of free time remaining
+		</div>
+		<div class="deadln">
+			before <strong>${deadlnTimeStr}</strong>,
+		</div>
+		<div class="deadln"><strong>
+			${deadlnDateStr}
+		</strong></div>
+	</div>
+	<div id="overlay"></div>
+	<div id="slideout">
+		<div id="pull_tab">
+			<img src="settings.png" />
+		</div>
+		<div id="slideout_inner">
+			<iframe src="settings.php" width="100%" seamless>Looks like iframes aren't supported by your browser. You should really consider upgrading.</iframe>
+		</div>
+	</div>
 </body>
 EOPAGE;
 echo $pagecontents;
---
+++ thestuff/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+$pageContents = <<<EOPAGE
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css" />
+	</head>
+	<body id="setting_panel">Settings</body>
+</html>
+EOPAGE;
+
+echo $pageContents;
+
+?>
